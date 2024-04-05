197c7048a7184452907a297ae6f6e1e57fe93338
GrantDev <grant.dev14@gmail.com>
2024-04-05 20:04:10 +0800

    Frontend-Test commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-console.log('loading index.js file...')
+$(document).ready(function(){
+    console.log('page load ready');
+    function initializeSlider() {
+      jQuery('.slider').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000, // Adjust autoplay speed as needed
+          dots: true, // Show navigation dots
+          responsive: [
+              {
+                  breakpoint: 991,
+                  settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1,
+                      autoplay: true,
+                      autoplaySpeed: 2000, // Adjust autoplay speed as needed
+                      dots: true, // Show navigation dots
+                  }
+              }
+          ]
+      });
+  }
+  function destroySlider() {
+      jQuery('.slider').slick('unslick');
+  }
+
+  // Initialize or destroy slick slider based on window width on page load
+  var windowWidth = $(window).width();
+  if (windowWidth < 991) {
+      initializeSlider();
+  } else {
+    
+  }
+
+  // Check window width on resize
+  jQuery(window).resize(function(){
+      var windowWidth = $(window).width();
+      if (windowWidth < 991) {
+          initializeSlider();
+      } else {
+          
+      }
+  });
+
+});
